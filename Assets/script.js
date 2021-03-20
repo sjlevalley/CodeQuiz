4b552b9f84914fe1f7530c1f325d5378aaa7378d
Steve LeValley <slevalley@gmail.com>
2021-03-20 16:19:16 -0700

    Timer working when start clicked

--- Assets/script.js
+++ Assets/script.js
@@ -1,8 +1,10 @@
 
 var header = document.querySelector(".header");
 var startPage = document.querySelector(".startPage");
+
 var startQuizButton = document.querySelector(".startQuizButton");
 var quizPage = document.querySelector(".quizPage");
+var secRemain = document.querySelector(".secRemain");
 var question = document.querySelector(".question");
 var answerList = document.querySelector(".answerList");
 var answer = document.querySelector(".answer");
@@ -12,24 +14,22 @@ var incorrect = document.querySelector(".incorrect");
 var quizEndButtons = document.querySelector(".quizEndButtons");
 var takeQuizAgain = document.querySelector(".takeQuizAgain");
 var clearHighScores = document.querySelector(".clearHighScores");
-var secRemain = 75
+var hasWon = false;
 
 
+function setTime(hasWon) {
+    var secLeft = 75;
+        var timeInterval = setInterval(function () {
+            secLeft--;
+          secRemain.textContent = secLeft;
+          if(secLeft === 0 || hasWon === true) {
+            clearInterval(timeInterval);
+            secRemain.textContent= ("Time is Up!");
+          }
+        }, 1000);}
 
-
-// ################# Timer Function ###############
-function myTimer() {
-    secRemain--;
-    secRemain.textContent = secRemain;
-};
+startQuizButton.addEventListener("click", setTime);
 
 
 
 // ############# When Start Button Clicked ##############
-
-startQuizButton.addEventListener("click", function() {
-    
-    quizPage.setAttribute.display = 'none';
-  });
-
-
