1724ad469c8e791e4b4c3aa64a36c04ef0d0f21b
Steve LeValley <slevalley@gmail.com>
2021-03-20 15:18:29 -0700

    Start

--- Assets/script.js
+++ Assets/script.js
@@ -1,13 +1,35 @@
 
+var header = document.querySelector(".header");
+var startPage = document.querySelector(".startPage");
+var startQuizButton = document.querySelector(".startQuizButton");
+var quizPage = document.querySelector(".quizPage");
+var question = document.querySelector(".question");
+var answerList = document.querySelector(".answerList");
+var answer = document.querySelector(".answer");
+var correctIncorrectBox = document.querySelector(".correctIncorrectBox");
+var correct = document.querySelector(".correct");
+var incorrect = document.querySelector(".incorrect");
+var quizEndButtons = document.querySelector(".quizEndButtons");
+var takeQuizAgain = document.querySelector(".takeQuizAgain");
+var clearHighScores = document.querySelector(".clearHighScores");
+var secRemain = 75
 
 
-var quizMainBox = document.querySelector(".quizMainBox");
-var viewHighScores = document.querySelector(".viewHighScores");
-var cardTimer = document.querySelector(".cardTimer");
-var timerText = document.querySelector(".timerText");
-var secRemain = document.querySelector(".secRemain");
-var quizMainBox = document.querySelector(".quizMainBox");
-var questionBox = document.querySelector(".questionBox");
-var startQuiz = document.querySelector(".startQuiz");
+
+
+// ################# Timer Function ###############
+function myTimer() {
+    secRemain--;
+    secRemain.textContent = secRemain;
+};
+
+
+
+// ############# When Start Button Clicked ##############
+
+startQuizButton.addEventListener("click", function() {
+    
+    quizPage.setAttribute.display = 'none';
+  });
 
 
